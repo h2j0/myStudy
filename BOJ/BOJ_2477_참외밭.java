e5aea60a361a138fe499966ad0790e7d7f86f2b0
Heejeong <97885766+h2j0@users.noreply.github.com>
2022-02-24 01:33:26 +0900

    Update BOJ_2477_참외밭.java

--- BOJ/BOJ_2477_참외밭.java
+++ BOJ/BOJ_2477_참외밭.java
@@ -5,7 +5,7 @@ public class BOJ_2477_참외밭 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int mul = sc.nextInt();
-		int[] arr = new int[10]; // 앞, 뒤 2칸의 여유를 주기 위해 10칸 만든다
+		int[] arr = new int[6]; // 앞, 뒤 2칸의 여유를 주기 위해 10칸 만든다
 
 		// 밭 정보 입력받음
 		// 입력 받으면서 최대값이랑 그 인덱스 저장하기
@@ -13,20 +13,16 @@ public class BOJ_2477_참외밭 {
 		int maxIdx = 0;
 		for (int i = 0; i < 6; i++) {
 			sc.nextInt();
-			arr[i + 2] = sc.nextInt();
-			max = Math.max(max, arr[i + 2]);
-			if (arr[i + 2] == max) {
-				maxIdx = i + 2;
+			arr[i] = sc.nextInt();
+			max = Math.max(max, arr[i]);
+			if (arr[i] == max) {
+				maxIdx = i;
 			}
 		}
-		// 인덱스 오류를 고려해 실제 배열의 앞 뒤 2칸에
-		// 수가 반복되도록? 값을 넣어준다
-		arr[0] = arr[6];
-		arr[1] = arr[7];
-		arr[8] = arr[2];
-		arr[9] = arr[3];
-		System.out.println(Arrays.toString(arr));
-		int area = mul * (arr[maxIdx - 1] * arr[maxIdx - 2] + arr[maxIdx + 1] * arr[maxIdx + 2]);
+		
+		// 최대값의 앞에 두개 뒤에 두개를 최대값과 곱해준다
+		int area = mul
+				* (arr[(maxIdx + 5) % 6] * arr[(maxIdx + 4) % 6] + arr[(maxIdx + 1) % 6] * arr[(maxIdx + 2) % 6]);
 		System.out.println(area);
 	}
 }
