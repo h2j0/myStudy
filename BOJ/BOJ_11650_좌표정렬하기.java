d4595dbaf3e21bfddd8d096117a21eb831cef096
h2j0 <heejeong0527@gmail.com>
2022-03-12 13:31:08 +0900

    Create BOJ_11650_좌표정렬하기.java

---
+++ BOJ/BOJ_11650_좌표정렬하기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class BOJ_11650_좌표정렬하기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		int[][] numArr = new int[n][2];
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			numArr[i][0] = Integer.parseInt(st.nextToken());
+			numArr[i][1] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(numArr, new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if (o1[0] == o2[0])
+					return Integer.compare(o1[1], o2[1]);
+				return Integer.compare(o1[0], o2[0]);
+			}
+		});
+
+		for (int i = 0; i < n; i++) {
+			sb.append(numArr[i][0]).append(" ");
+			sb.append(numArr[i][1]).append("\n");
+		}
+		System.out.println(sb);
+
+	}
+}
